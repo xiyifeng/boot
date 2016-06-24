556a0e34a2f1445ebf80ac74d58208cc448bfa50
xiyifeng <Administrator@PC201606171017>
2016-06-24 08:39:55 +0800

    nothing commit;

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -100,7 +100,7 @@ function addItem(title, url, clsClass) {
 		$('#main_tabs').tabs('select', title);
 	} else {
 		var content = '<iframe scrolling="auto" frameborder="0"  src="'
-				+ url + '" style="width:100%;height:100%;"></iframe>';
+				+ url + '" style="width:100%;height:98%;"></iframe>';
 		$('#main_tabs').tabs('add', {
 			title : title,
 			content : content,
